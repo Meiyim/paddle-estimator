58fa8232e95b639809cc804d90890c890abeedc1
chenxuyi <chenxuyi@baidu.com>
2019-08-14 15:49:14 +0800

    + BertInferenceModelExporter

--- atarashi/paddle/train/exporter.py
+++ atarashi/paddle/train/exporter.py
@@ -54,30 +54,46 @@ class BestExporter(Exporter):
             return
         if self.cmp_fn(old=self._best, new=eval_result):
             log.debug('[Best Exporter]: export to %s' % self._export_dir)
-            if eval_model_spec.inference_spec is not None:
-                inf_sepc_dict = eval_model_spec.inference_spec
-                if not isinstance(inf_sepc_dict, dict):
-                    inf_sepc_dict = {'inference': inf_sepc_dict}
-                for inf_sepc_name, inf_sepc in six.iteritems(inf_sepc_dict):
-                    if not isinstance(inf_sepc, InferenceSpec):
-                        raise ValueError('unkonw inference spec type: %s' % v)
+            saver = Saver(
+                self._export_dir, exe, program=program, max_ckpt_to_keep=1)
+            saver.save(state)
+            self._best = eval_result
+        else:
+            log.debug('[Best Exporter]: skip step %s' % state.gstep)
 
-                    save_dir = os.path.join(self._export_dir, inf_sepc_name)
-                    log.debug('[Best Exporter]: save inference model: %s to %s'
-                              % (inf_sepc_name, save_dir))
-                    feed_var = [i.name for i in inf_sepc.inputs]
-                    fetch_var = inf_sepc.outputs
-                    F.io.save_inference_model(
-                        save_dir,
-                        feed_var,
-                        fetch_var,
-                        exe,
-                        main_program=program)
-            else:
-                log.debug('[Best Exporter]: save regular model')
-                saver = Saver(
-                    self._export_dir, exe, program=program, max_ckpt_to_keep=1)
-                saver.save(state)
+
+class BestInferenceModelExporter(Exporter):
+    def __init__(self, export_dir, cmp_fn):
+        self._export_dir = export_dir
+        self._best = None
+        self.cmp_fn = cmp_fn
+
+    def export(self, exe, program, eval_model_spec, eval_result, state):
+        log.debug('New evaluate result: %s \nold: %s' %
+                  (repr(eval_result), repr(self._best)))
+        if self._best is None:
+            self._best = eval_result
+            log.debug('[Best Exporter]: skip step %d' % state.gstep)
+            return
+        if self.cmp_fn(old=self._best, new=eval_result):
+            log.debug('[Best Exporter]: export to %s' % self._export_dir)
+            if eval_model_spec.inference_spec is not None:
+                raise ValueError('model_fn didnt return InferenceSpec')
+
+            inf_sepc_dict = eval_model_spec.inference_spec
+            if not isinstance(inf_sepc_dict, dict):
+                inf_sepc_dict = {'inference': inf_sepc_dict}
+            for inf_sepc_name, inf_sepc in six.iteritems(inf_sepc_dict):
+                if not isinstance(inf_sepc, InferenceSpec):
+                    raise ValueError('unkonw inference spec type: %s' % v)
+
+                save_dir = os.path.join(self._export_dir, inf_sepc_name)
+                log.debug('[Best Exporter]: save inference model: %s to %s' %
+                          (inf_sepc_name, save_dir))
+                feed_var = [i.name for i in inf_sepc.inputs]
+                fetch_var = inf_sepc.outputs
+                F.io.save_inference_model(
+                    save_dir, feed_var, fetch_var, exe, main_program=program)
             self._best = eval_result
         else:
             log.debug('[Best Exporter]: skip step %s' % state.gstep)
